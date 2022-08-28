ac540d27b9f3892c1dc4f8bc6a9f54354b1f7a46
andriy andrusenko <andrusenkone@gmail.com>
2022-08-28 22:08:13 +0300

    http v. 0.2

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+export function fetchCountries(name) {
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+  return fetch(url).then(d => {
+    if (!d.ok) {
+      throw new Error(d.status);
+    }
+    return d.json();
+  });
+}
